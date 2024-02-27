c9583669afe5a9846b3d8e9ff431515cb6ea486f
MoeNya <i@moenya.cat>
2024-02-27 15:31:29 +0800

    Update navigation model

--- gin-server/gin-blog-server/model/navigation.go
+++ gin-server/gin-blog-server/model/navigation.go
@@ -7,3 +7,11 @@ type Navigation struct {
 	Navtitle string `gorm:"type:varchar(20);not null" json:"navtitle" label:"导航栏标题"`
 	Navurl   string `gorm:"type:varchar(1000)" json:"navurl" label:"导航链接"`
 }
+
+// 增加导航标签
+
+// 获取导航标签列表
+
+// 修改导航标签
+
+// 删除导航标签
