f89fd1e759ba1e2869979f7168c8f9fe332ba48b
Moe_Nya <i@moenya.cat>
2024-02-28 17:31:58 +0800

    Update findme model

--- gin-server/gin-blog-server/model/findme.go
+++ gin-server/gin-blog-server/model/findme.go
@@ -4,7 +4,16 @@ import "gorm.io/gorm"
 
 type FindMe struct {
 	gorm.Model
-	Icon string `gorm:"type:varchar(1000);not null" json:"icon" label:"图标"`
-	Herf string `gorm:"type:varchar(1000)not null" json:"herf" label:"链接"`
-	Text string `gorm:"type:varchar(50)" json:"text" label:"文本"`
+	FindId int    `gorm:"type:int(10);not null" json:"findid" label:"找到我ID"`
+	Icon   string `gorm:"type:varchar(1000);not null" json:"icon" label:"图标"`
+	Herf   string `gorm:"type:varchar(1000)not null" json:"herf" label:"链接"`
+	Text   string `gorm:"type:varchar(50)" json:"text" label:"文本"`
 }
+
+// 新增Findme
+
+// 查询所有Findme
+
+// 修改Findme
+
+// 删除Findme
