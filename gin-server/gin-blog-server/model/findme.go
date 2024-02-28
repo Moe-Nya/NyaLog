859d2bcac252d7c7b768bafd328bcceccd6614d8
Moe_Nya <i@moenya.cat>
2024-02-28 18:40:38 +0800

    Update findme model

--- gin-server/gin-blog-server/model/findme.go
+++ gin-server/gin-blog-server/model/findme.go
@@ -1,6 +1,10 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"NyaLog/gin-blog-server/utils/errmsg"
+
+	"gorm.io/gorm"
+)
 
 type FindMe struct {
 	gorm.Model
@@ -11,9 +15,64 @@ type FindMe struct {
 }
 
 // 新增Findme
+func CreateFindme(data *FindMe) int {
+	var findme FindMe
+	findme.Icon = data.Icon
+	findme.Herf = data.Herf
+	findme.Text = data.Text
+	var count int64
+	var f FindMe
+	err := db.Find(&f).Count(&count).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+	if count == 0 {
+		findme.FindId = 0
+	} else {
+		var finalfind FindMe
+		err = db.Limit(1).Offset(int(count - 1)).Find(&finalfind).Error
+		if err != nil {
+			return errmsg.ERROR
+		}
+		findme.FindId = finalfind.FindId + 1
+	}
+	err = db.Create(&findme).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCESS
+}
 
 // 查询所有Findme
+func SeleFindme() ([]FindMe, int) {
+	var findme []FindMe
+	err := db.Find(&findme).Error
+	if err != nil {
+		return findme, errmsg.ERROR
+	}
+	return findme, errmsg.SUCCESS
+}
 
 // 修改Findme
+func ModifyFindme(data *FindMe) int {
+	var findme FindMe
+	var findmap = make(map[string]interface{})
+	findmap["icon"] = data.Icon
+	findmap["herf"] = data.Herf
+	findmap["text"] = data.Text
+	err := db.Model(&findme).Where("find_id = ?", data.FindId).Updates(findmap).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCESS
+}
 
 // 删除Findme
+func DeleFindme(findid int) int {
+	var findme FindMe
+	err := db.Where("find_id = ?", findid).Unscoped().Delete(&findme).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCESS
+}
