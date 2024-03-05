8b461ae3525a0a5ce224453f7dd4ad2a6b9fd60e
MoeNya <i@moenya.cat>
2024-03-05 17:40:03 +0800

    Update blogset model test file

--- gin-server/test/ModelTest/blogsetmodel/modifyblogset_test.go
+++ gin-server/test/ModelTest/blogsetmodel/modifyblogset_test.go
@@ -4,11 +4,13 @@ import (
 	"NyaLog/gin-blog-server/model"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestModifyBlogSet(t *testing.T) {
 	model.InitDb()
 	var blogset = &model.BlogSet{}
+	blogset.Sitecreatetime = time.Now()
 	blogset.Sitename = "猫猫头"
 	blogset.Aiswitch = 0
 	blogset.Aicategory = 1
