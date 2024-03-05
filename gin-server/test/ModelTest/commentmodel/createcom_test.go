468ca3bcdeda34496a220435c00b171a57dd253c
MoeNya <i@moenya.cat>
2024-03-05 23:16:54 +0800

    Update comment model test file

---
+++ gin-server/test/ModelTest/commentmodel/createcom_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"NyaLog/gin-blog-server/model"
+	"fmt"
+	"testing"
+)
+
+func TestCreateCom(t *testing.T) {
+	model.InitDb()
+	var com = &model.Comment{}
+	com.Articleid = 1
+	com.Userid = "moenya"
+	com.Profilephoto = "www.google.com"
+	com.Usersite = "www.youtube.com"
+	com.Comment = "测试回复2"
+	com.Recomid = "0"
+	fmt.Println(model.CreateCom(com))
+}
