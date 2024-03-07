12443d53551b6aaf87e35e663f511131e6a85d24
MoeNya <i@moenya.cat>
2024-03-07 22:36:07 +0800

    Create user service

---
+++ gin-server/gin-blog-server/service/user.go
@@ -0,0 +1,16 @@
+package service
+
+import (
+	"NyaLog/gin-blog-server/model"
+	"NyaLog/gin-blog-server/utils/errmsg"
+)
+
+// 创建用户
+// todo 先解决谷歌二级验证码中间件、登录安全中间件、token内存保持服务
+func CreateUser(user *model.User) int {
+	if user.Uid == "" || user.Username == "" || user.Password == "" || user.Email == "" || user.Secret == "" {
+		return errmsg.UserInfoError
+	}
+
+	return errmsg.SUCCESS
+}
