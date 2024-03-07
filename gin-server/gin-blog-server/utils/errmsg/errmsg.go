6323d410e2c4de9f1db48d8e0540841b08a2d8bf
MoeNya <i@moenya.cat>
2024-03-07 17:15:29 +0800

    Add jwt error message

--- gin-server/gin-blog-server/utils/errmsg/errmsg.go
+++ gin-server/gin-blog-server/utils/errmsg/errmsg.go
@@ -3,11 +3,19 @@ package errmsg
 const (
 	SUCCESS = 200
 	ERROR   = 500
+
+	// JWT中间件
+	TokenNotExist    = 1001
+	TokenInvalid     = 1002
+	TokenParseFailed = 1003
 )
 
 var codeMsg = map[int]string{
-	SUCCESS: "ok",
-	ERROR:   "error",
+	SUCCESS:          "ok",
+	ERROR:            "error",
+	TokenNotExist:    "token not exist",
+	TokenInvalid:     "token invalid",
+	TokenParseFailed: "failed to parse claims",
 }
 
 func GetErrorMsg(code int) string {
