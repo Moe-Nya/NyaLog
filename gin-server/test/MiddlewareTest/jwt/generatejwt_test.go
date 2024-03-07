ff38516145cd2ad6e2e94b6ca8bd61c36786b5d3
MoeNya <i@moenya.cat>
2024-03-07 17:16:05 +0800

    Update jwt test file

---
+++ gin-server/test/MiddlewareTest/jwt/generatejwt_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"NyaLog/gin-blog-server/middleware"
+	"fmt"
+	"testing"
+)
+
+func TestGenerateJwt(t *testing.T) {
+	var user = &middleware.UserClaims{}
+	user.Uid = "moenya"
+	user.Uip = "127.0.0.1"
+	token, _ := middleware.GenerateJWT(user)
+
+	fmt.Println(middleware.ValidateJWT(token, user.Uid, user.Uip))
+}
