db3045523e06daaad1abb201f0c523312fafd9f1
MoeNya <i@moenya.cat>
2024-03-08 22:46:17 +0800

    Update error message

--- gin-server/gin-blog-server/utils/errmsg/errmsg.go
+++ gin-server/gin-blog-server/utils/errmsg/errmsg.go
@@ -5,12 +5,23 @@ const (
 	ERROR   = 500
 
 	// JWT中间件
-	TokenNotExist    = 1001
-	TokenInvalid     = 1002
-	TokenParseFailed = 1003
+	TokenNotExist       = 1001
+	TokenInvalid        = 1002
+	TokenParseFailed    = 1003
+	TokenGenerateFailed = 1004
+
+	// 2FA
+	GenerateSecretFailed = 3001
+	CodeError            = 3002
+
+	// QRcode
+	GenerateQRFailed = 4001
 
 	// 用户登录
-	UserInfoError = 2001
+	UserInfoError   = 2001
+	UserExist       = 2002
+	UserNotExist    = 2003
+	CheckUserFailed = 2004
 )
 
 var codeMsg = map[int]string{
@@ -18,10 +29,23 @@ var codeMsg = map[int]string{
 	ERROR:   "error",
 
 	// JWT中间件
-	TokenNotExist:    "token not exist",
-	TokenInvalid:     "token invalid",
-	TokenParseFailed: "failed to parse claims",
-	UserInfoError:    "uid or password error",
+	TokenNotExist:       "token not exist",
+	TokenInvalid:        "token invalid",
+	TokenParseFailed:    "failed to parse claims",
+	TokenGenerateFailed: "token generate failed",
+
+	// 2FA
+	GenerateSecretFailed: "generate secret failed",
+	CodeError:            "code error",
+
+	// QRcode
+	GenerateQRFailed: "generate qrcode failed",
+
+	// 管理员用户
+	UserInfoError:   "user information error",
+	UserExist:       "user exist",
+	UserNotExist:    "user not exist",
+	CheckUserFailed: "check user failed",
 }
 
 func GetErrorMsg(code int) string {
