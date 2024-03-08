d9f7b1b2b0d6f1912a47cbad6adc4313abdb2bb0
MoeNya <i@moenya.cat>
2024-03-08 16:12:28 +0800

    Update 2FA function

--- gin-server/gin-blog-server/middleware/2FA.go
+++ gin-server/gin-blog-server/middleware/2FA.go
@@ -8,13 +8,23 @@ import (
 
 // two-factor authentication
 
+// 生成2FA
 func GenerateTwoFA(uid string) (string, int) {
 	key, err := totp.Generate(totp.GenerateOpts{
 		Issuer:      "NyaLog",
 		AccountName: uid,
 	})
 	if err != nil {
-		return "", errmsg.ERROR
+		return "", errmsg.GenerateSecretError
 	}
-	return key.Secret(), errmsg.SUCCESS
+	return key.URL(), errmsg.SUCCESS
+}
+
+// 验证2FA
+func Validate2FA(code string, secret string) int {
+	validate := totp.Validate(code, secret)
+	if !validate {
+		return errmsg.CodeError
+	}
+	return errmsg.SUCCESS
 }
