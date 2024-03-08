e08f10b4d46758a7cfb17acc110a8cf58c8bddf0
MoeNya <i@moenya.cat>
2024-03-08 22:47:21 +0800

    Update user model test file

--- gin-server/test/ModelTest/usermodel/updateuser_test.go
+++ gin-server/test/ModelTest/usermodel/updateuser_test.go
@@ -8,8 +8,8 @@ import (
 
 func Test_Update(t *testing.T) {
 	model.InitDb()
-	var user model.User
-	user, _ = model.SeleUser()
+	var user = &model.User{}
+	*user, _ = model.SeleUser()
 	user.Username = "MoeNya"
 	code := model.UpdateUser(user.Uid, user)
 	fmt.Println(code)
