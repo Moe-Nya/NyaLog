05f91676bacd0b4bb2e0bd6265e94ce93733447a
MoeNya <i@moenya.cat>
2024-03-08 22:43:26 +0800

    Update router

--- gin-server/gin-blog-server/api/v1/user.go
+++ gin-server/gin-blog-server/api/v1/user.go
@@ -1,13 +1,64 @@
 package v1
 
 import (
+	"NyaLog/gin-blog-server/model"
+	"NyaLog/gin-blog-server/service"
+	"NyaLog/gin-blog-server/utils/errmsg"
+	"encoding/base64"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-func StartServer(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"msg": 111,
-	})
+// 判断用户是否存在
+func UserExist(c *gin.Context) {
+	userexist, err := service.UserExist()
+	if err == errmsg.ERROR {
+		c.JSON(http.StatusOK, gin.H{
+			"code":    errmsg.ERROR,
+			"message": "User system error",
+		})
+	}
+	if userexist {
+		c.JSON(http.StatusOK, gin.H{
+			"code":    errmsg.ERROR,
+			"message": "User exist",
+		})
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"code": errmsg.SUCCESS,
+		})
+	}
+}
+
+// 注册用户
+func CreateUser(c *gin.Context) {
+	var data model.User
+	_ = c.ShouldBindJSON(&data)
+	qrcode, err := service.CreateUser(&data)
+	if err == errmsg.ERROR {
+		c.JSON(http.StatusOK, gin.H{
+			"code":    errmsg.ERROR,
+			"message": "User registration failed",
+		})
+	}
+	if err == errmsg.UserExist {
+		c.JSON(http.StatusOK, gin.H{
+			"code":    errmsg.UserExist,
+			"message": errmsg.GetErrorMsg(errmsg.UserExist),
+		})
+	}
+	if err == errmsg.UserInfoError {
+		c.JSON(http.StatusOK, gin.H{
+			"code":    errmsg.UserInfoError,
+			"message": errmsg.GetErrorMsg(errmsg.UserInfoError),
+		})
+	}
+	if err == errmsg.SUCCESS {
+		c.JSON(http.StatusOK, gin.H{
+			"code":    errmsg.SUCCESS,
+			"message": "User registration success",
+			"qrcode":  base64.StdEncoding.EncodeToString(qrcode),
+		})
+	}
 }
