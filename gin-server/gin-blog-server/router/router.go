94ea4b873a08a3f01be5d637363997eea457dcc8
MoeNya <i@moenya.cat>
2024-03-08 22:45:22 +0800

    Update router

--- gin-server/gin-blog-server/router/router.go
+++ gin-server/gin-blog-server/router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	v1 "NyaLog/gin-blog-server/api/v1"
+	"NyaLog/gin-blog-server/middleware"
 	"NyaLog/gin-blog-server/utils"
 
 	"github.com/gin-gonic/gin"
@@ -11,8 +12,16 @@ func IniRouter() {
 	gin.SetMode(utils.AppMode)
 	routerv1 := gin.Default()
 
-	r1 := routerv1.Group("api/v1")
-	r1.GET("hello", v1.StartServer)
+	normalrouter := routerv1.Group("api/v1")
+	// 用户是否存在
+	normalrouter.GET("/adminexist", v1.UserExist)
+	normalrouter.POST("/adminregistration", v1.CreateUser)
+
+	authrouter := routerv1.Group("api/v1")
+	authrouter.Use(middleware.JwtToken())
+	{
+
+	}
 
 	_ = routerv1.Run(utils.HttpPort)
 }
