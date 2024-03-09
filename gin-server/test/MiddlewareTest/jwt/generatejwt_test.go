27419023f50d53a304a3e057b7ca91c31d17024e
MoeNya <i@moenya.cat>
2024-03-09 15:01:15 +0800

    Delete email model

--- gin-server/test/MiddlewareTest/jwt/generatejwt_test.go
+++ gin-server/test/MiddlewareTest/jwt/generatejwt_test.go
@@ -4,13 +4,15 @@ import (
 	"NyaLog/gin-blog-server/middleware"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestGenerateJwt(t *testing.T) {
 	var user = &middleware.UserClaims{}
 	user.Uid = "moenya"
 	user.Uip = "127.0.0.1"
-	token, _ := middleware.GenerateJWT(user)
+	var tt time.Duration = time.Hour
+	token, _ := middleware.GenerateJWT(user, tt)
 
 	fmt.Println(middleware.ValidateJWT(token, user.Uip))
 }
