95a2afd9bbc9fd5badb8b240777bc8798a68bb51
MoeNya <i@moenya.cat>
2024-03-12 19:32:20 +0800

    Update router

--- gin-server/gin-blog-server/router/router.go
+++ gin-server/gin-blog-server/router/router.go
@@ -15,15 +15,24 @@ func IniRouter() {
 	normalrouter := routerv1.Group("api/v1")
 	// 用户是否存在
 	normalrouter.GET("/adminexist", v1.UserExist)
+
+	// 注册用户
 	normalrouter.POST("/adminregistration", v1.CreateUser)
+
+	// 用户登录
+	normalrouter.POST("/login", v1.UserLogin)
 
 	authrouter := routerv1.Group("api/v1")
 	authrouter.Use(middleware.JwtToken())
 	{
 		// 发送邮件验证码
 		authrouter.GET("/sendemail", v1.SendEmailCode)
+
 		// 验证用户注册信息
 		authrouter.POST("/adminvalidate", v1.ValidateUser)
+
+		// 登录注销
+		authrouter.POST("/loginout", v1.UserLoginOut)
 	}
 
 	_ = routerv1.Run(utils.HttpPort)
