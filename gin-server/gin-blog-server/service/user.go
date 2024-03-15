07bdbba8a81f334538e2988c8eb625acd3109cf3
MoeNya <i@moenya.cat>
2024-03-15 22:54:55 +0800

    Update service

--- gin-server/gin-blog-server/service/user.go
+++ gin-server/gin-blog-server/service/user.go
@@ -15,6 +15,65 @@ func UserExist() (bool, int) {
 	return model.UserExist()
 }
 
+// -------------------------------------------
+
+// 发送邮箱时需要的结构体
+type SendEmail struct {
+	Uid      string `json:"uid"`
+	Email    string `json:"email"`
+	Useplace string `json:"useplace"`
+	Category bool   `json:"category"`
+}
+
+// 发送邮件验证码
+// 传值false为注册时验证用户， true为找回密码所用
+func SendEmailCode(data *SendEmail) int {
+	// 验证用户是否已经存在
+	userexist, err := UserExist()
+	if err == errmsg.ERROR {
+		return err
+	}
+	if userexist {
+		uservalidate, err := model.UserValidate()
+		if err == errmsg.ERROR {
+			return err
+		}
+		if uservalidate && !data.Category {
+			return errmsg.UserExist
+		}
+	} else {
+		return errmsg.UserNotExist
+	}
+
+	var user model.User
+	user, err = model.SeleUser()
+	if err == errmsg.ERROR {
+		return err
+	}
+	if data.Email == user.Email {
+		// 生成邮件验证码并且发送邮件信息
+		rand.NewSource(time.Now().UnixNano())
+		code := make([]byte, 6)
+		for i := 0; i < 6; i++ {
+			code[i] = byte(rand.Intn(10) + '0')
+		}
+		// 编辑发送邮件的信息
+		msg := []byte("From: " + "NyaLog" + "\r\n" +
+			"To: " + user.Email + "\r\n" +
+			"Subject: " + "NyaLog: Your verification code is:" + "\r\n" +
+			"\r\n" +
+			string(code))
+		err = middleware.SendEmail(user.Email, msg)
+		if err != errmsg.SUCCESS {
+			return err
+		}
+		middleware.CheckUserEmailCode(user.Uid, data.Useplace, string(code))
+	}
+	return errmsg.SUCCESS
+}
+
+// -------------------------------------------
+
 // 创建用户
 func CreateUser(user *model.User) ([]byte, int) {
 	// 验证用户是否已经存在
@@ -60,50 +119,6 @@ func CreateUser(user *model.User) ([]byte, int) {
 	return qrcode, errmsg.SUCCESS
 }
 
-// 发送邮件验证码
-func SendEmailCode() int {
-	// 验证用户是否已经存在
-	userexist, err := UserExist()
-	if err == errmsg.ERROR {
-		return err
-	}
-	if userexist {
-		uservalidate, err := model.UserValidate()
-		if err == errmsg.ERROR {
-			return err
-		}
-		if uservalidate {
-			return errmsg.UserExist
-		}
-	} else {
-		return errmsg.UserNotExist
-	}
-
-	var user model.User
-	user, err = model.SeleUser()
-	if err == errmsg.ERROR {
-		return err
-	}
-	// 生成邮件验证码并且发送邮件信息
-	rand.NewSource(time.Now().UnixNano())
-	code := make([]byte, 6)
-	for i := 0; i < 6; i++ {
-		code[i] = byte(rand.Intn(10) + '0')
-	}
-	// 编辑发送邮件的信息
-	msg := []byte("From: " + "NyaLog" + "\r\n" +
-		"To: " + user.Email + "\r\n" +
-		"Subject: " + "NyaLog: Your verification code is:" + "\r\n" +
-		"\r\n" +
-		string(code))
-	err = middleware.SendEmail(user.Email, msg)
-	if err != errmsg.SUCCESS {
-		return err
-	}
-	middleware.CheckUserEmailCode(user.Uid, string(code))
-	return errmsg.SUCCESS
-}
-
 // 注册用户时验证用户需要用到的结构体
 type CheckUserToken struct {
 	Emailcode string `json:"emailcode"`
@@ -135,9 +150,12 @@ func CheckUser(data *CheckUserToken, token string) int {
 
 	// 验证用户输入的验证信息
 	uid, _ := middleware.ValidateJWT(token, data.Userip)
-	code, err := middleware.GetCode(uid)
+	code, useplace, err := middleware.GetCode(uid)
 	if err != errmsg.SUCCESS || code != data.Emailcode {
-		return errmsg.UserEmailCodeError
+		return errmsg.ValidateCodeError
+	}
+	if useplace != "create" {
+		return errmsg.ValidateCodeError
 	}
 	if middleware.Validate2FA(data.Twofacode, user.Secret) == errmsg.CodeError {
 		return errmsg.CodeError
@@ -158,6 +176,8 @@ type Ulogin struct {
 	Password  string `json:"password"`
 	TwoFACode string `json:"twofacode"`
 }
+
+// -------------------------------------------
 
 // 用户登录
 func UserLogin(data *Ulogin) int {
@@ -208,6 +228,59 @@ func UserLoginOut(uid string) int {
 	err := middleware.DeleteToken(uid)
 	if err != errmsg.SUCCESS {
 		return errmsg.TokenNotExist
+	}
+	return errmsg.SUCCESS
+}
+
+// -------------------------------------------
+
+// 重置密码时所用的结构体
+type ValidateRe struct {
+	Uid       string `json:"uid"`
+	Email     string `json:"email"`
+	TwoFaCode string `json:"twofacode"`
+}
+
+// 重置密码
+func ValidateReset(data *ValidateRe) int {
+	var user model.User
+	user, err := model.SeleUser()
+	if err != errmsg.SUCCESS {
+		return errmsg.ResetPasswordFailed
+	}
+	if data.Uid != user.Uid || data.Email != user.Email {
+		return errmsg.UserInfoError
+	} else {
+		err = middleware.Validate2FA(data.TwoFaCode, user.Secret)
+		if err != errmsg.SUCCESS {
+			return err
+		}
+	}
+	return errmsg.SUCCESS
+}
+
+// 重置密码验证邮箱所用结构体
+type ValidateEm struct {
+	Uid      string `json:"uid"`
+	Password string `json:"password"`
+	Code     string `json:"code"`
+}
+
+// 验证邮箱
+func ValidateEmail(data *ValidateEm) int {
+	code, useplace, err := middleware.GetCode(data.Uid)
+	if err != errmsg.SUCCESS {
+		return errmsg.ValidateCodeError
+	}
+	if code != data.Code {
+		return errmsg.ValidateCodeError
+	}
+	if useplace != "reset" {
+		return errmsg.ValidateCodeError
+	}
+	err = model.ModifyPw(data.Password)
+	if err != errmsg.SUCCESS {
+		return errmsg.ResetPasswordFailed
 	}
 	return errmsg.SUCCESS
 }
