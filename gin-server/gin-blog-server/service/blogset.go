1ef496c2b79b47d9dd3ed3543d9dabcce9240abe
MoeNya <i@moenya.cat>
2024-03-17 23:26:45 +0800

    Create blog set service

---
+++ gin-server/gin-blog-server/service/blogset.go
@@ -0,0 +1,39 @@
+package service
+
+import (
+	"NyaLog/gin-blog-server/model"
+	"NyaLog/gin-blog-server/utils/errmsg"
+	"fmt"
+)
+
+// 更新博客设置
+func UpdateBlogSet(data *model.BlogSet) int {
+	err, ex := model.ValidateBlogSet()
+	if err != errmsg.SUCCESS {
+		fmt.Println("这里错了！111")
+		return errmsg.BlogSetFailed
+	}
+	if ex == 0 {
+		e := model.CreateBlogSet(data)
+		if e != errmsg.SUCCESS {
+			fmt.Println("这里错了！222")
+			return errmsg.BlogSetFailed
+		}
+	} else {
+		e := model.ModifyBlogSet(data)
+		if e != errmsg.SUCCESS {
+			fmt.Println("这里错了！333")
+			return errmsg.BlogSetFailed
+		}
+	}
+	return errmsg.SUCCESS
+}
+
+// 读取博客设置
+func SeleBlogSet() (model.BlogSet, int) {
+	blogset, err := model.SeleBlogSet()
+	if err != errmsg.SUCCESS {
+		return blogset, errmsg.BlogNotSet
+	}
+	return blogset, errmsg.SUCCESS
+}
