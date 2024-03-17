d4512ba95a6131889c9c7a09df5e665052d23c3a
MoeNya <i@moenya.cat>
2024-03-17 23:25:07 +0800

    Create blog set api

---
+++ gin-server/gin-blog-server/api/v1/blogset.go
@@ -0,0 +1,51 @@
+package v1
+
+import (
+	"NyaLog/gin-blog-server/model"
+	"NyaLog/gin-blog-server/service"
+	"NyaLog/gin-blog-server/utils/errmsg"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// 更新博客设置
+func UpdateBlogSet(c *gin.Context) {
+	var data model.BlogSet
+	_ = c.ShouldBindJSON(&data)
+	err := service.UpdateBlogSet(&data)
+	if err != errmsg.SUCCESS {
+		c.JSON(http.StatusOK, gin.H{
+			"code":    err,
+			"message": errmsg.GetErrorMsg(err),
+		})
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"code":    err,
+			"message": "update success",
+		})
+	}
+}
+
+// 读取博客设置
+func SeleBlogSet(c *gin.Context) {
+	blogset, err := service.SeleBlogSet()
+	if err != errmsg.SUCCESS {
+		c.JSON(http.StatusOK, gin.H{
+			"code":    err,
+			"message": errmsg.GetErrorMsg(err),
+		})
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"code":           err,
+			"sitename":       blogset.Sitename,
+			"sitecreatetime": blogset.Sitecreatetime,
+			"sitebackground": blogset.Sitebackground,
+			"aiswitch":       blogset.Aiswitch,
+			"aicategory":     blogset.Aicategory,
+			"aiurl":          blogset.Aiurl,
+			"commentswitch":  blogset.Commentswitch,
+			"githuburl":      blogset.Githuburl,
+		})
+	}
+}
