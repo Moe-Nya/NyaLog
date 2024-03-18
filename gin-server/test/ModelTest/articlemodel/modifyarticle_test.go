a743a76db9d20903525e5b56828eadae968022eb
MoeNya <i@moenya.cat>
2024-03-18 10:52:17 +0800

    Update test file

--- gin-server/test/ModelTest/articlemodel/modifyarticle_test.go
+++ gin-server/test/ModelTest/articlemodel/modifyarticle_test.go
@@ -9,9 +9,10 @@ import (
 func TestModifyArticle(t *testing.T) {
 	model.InitDb()
 	var article = &model.Article{}
+	article.Articleid = 2
 	article.Articletitle = "新标题！"
 	article.Cid = 3
 	article.Aisummary = "321321"
 	article.Text = "新文本！"
-	fmt.Println(model.ModifyArticle(2, article))
+	fmt.Println(model.ModifyArticle(article))
 }
