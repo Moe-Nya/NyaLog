922be84b85e0f6858b03ca7d6fa254de32a6807e
MoeNya <i@moenya.cat>
2024-03-19 11:40:06 +0800

    Create category service

---
+++ gin-server/gin-blog-server/service/category.go
@@ -0,0 +1,43 @@
+package service
+
+import (
+	"NyaLog/gin-blog-server/model"
+	"NyaLog/gin-blog-server/utils/errmsg"
+)
+
+// 新增文章分类
+func CreateCategory(data *model.Category) int {
+	err := model.CreateCat(data)
+	if err != errmsg.SUCCESS {
+		return errmsg.CatCreateFailed
+	}
+	return errmsg.SUCCESS
+}
+
+// 查询文章分类
+func SeleCategory() ([]model.Category, int) {
+	var category []model.Category
+	category, err := model.SeleCat()
+	if err != errmsg.SUCCESS {
+		return category, errmsg.CatQueryFailed
+	}
+	return category, errmsg.SUCCESS
+}
+
+// 编辑文章分类
+func EditCategory(data *model.Category) int {
+	err := model.ModifyCat(data)
+	if err != errmsg.SUCCESS {
+		return errmsg.CatUpdateFailed
+	}
+	return errmsg.SUCCESS
+}
+
+// 删除文章分类
+func DeleteCategory(cid int) int {
+	err := model.DeleCat(cid)
+	if err != errmsg.SUCCESS {
+		return errmsg.CatDeleteFailed
+	}
+	return errmsg.SUCCESS
+}
