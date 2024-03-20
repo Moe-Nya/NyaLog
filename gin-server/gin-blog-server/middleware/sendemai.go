25fe70bc044f206cbad1549c7bebd09be4363537
MoeNya <i@moenya.cat>
2024-03-20 22:12:40 +0800

    Update send mail middleware

--- gin-server/gin-blog-server/middleware/sendemai.go
+++ gin-server/gin-blog-server/middleware/sendemai.go
@@ -8,12 +8,12 @@ import (
 
 func SendEmail(email string, msg []byte) int {
 	// 邮件服务器的认证信息
-	auth := smtp.PlainAuth("", utils.Emailusername, utils.Emailpassword, utils.Stmphost)
+	auth := smtp.PlainAuth("", utils.Emailusername, utils.Emailpassword, utils.Smtphost)
 
 	var users []string
 	users = append(users, email)
 	// 发送邮件
-	err := smtp.SendMail(utils.Stmphost+":"+utils.Stmpport, auth, utils.Emailusername, users, msg)
+	err := smtp.SendMail(utils.Smtphost+":"+utils.Smtpport, auth, utils.Emailusername, users, msg)
 	if err != nil {
 		return errmsg.SendEmailFailed
 	}
