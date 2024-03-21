0fd29344eb9dcd0066a800b921d11eb70608ec54
MoeNya <i@moenya.cat>
2024-03-21 23:02:01 +0800

    Update comment service

--- gin-server/gin-blog-server/service/comment.go
+++ gin-server/gin-blog-server/service/comment.go
@@ -20,7 +20,7 @@ func NewComment(data *Comment, token string) int {
 	}
 	var com = &model.Comment{}
 	com.Articleid = data.Articleid
-	com.Comment = data.Comment
+	com.Commenttext = data.Comment
 	com.Userid = userinfo["name"].(string)
 	com.Usersite = userinfo["html_url"].(string)
 	com.Profilephoto = userinfo["avatar_url"].(string)
@@ -31,4 +31,48 @@ func NewComment(data *Comment, token string) int {
 	return errmsg.SUCCESS
 }
 
+// 读取文章评论所用结构体
+type ArticleComments struct {
+	Articleid int64  `json:"articleid"`
+	Comid     string `json:"comid"`
+	PageSize  int    `json:"pagesize"`
+	PageNum   int    `json:"pagenum"`
+}
+
 // 读取评论列表(某篇文章)
+func SeleCom(data *ArticleComments) ([]model.Comment, int) {
+	var comment []model.Comment
+	comment, err := model.SeleCom(data.Articleid, data.PageSize, data.PageNum)
+	if err != errmsg.SUCCESS {
+		return comment, errmsg.SelectCommentFailed
+	}
+	return comment, errmsg.SUCCESS
+}
+
+// 读取评论列表(所有文章)
+func SeleAllCom(data *ArticleComments) ([]model.Comment, int, int64) {
+	var comment []model.Comment
+	comment, err, total := model.SeleAllCom(data.PageSize, data.PageNum)
+	if err != errmsg.SUCCESS {
+		return comment, errmsg.SelectCommentFailed, 0
+	}
+	return comment, errmsg.SUCCESS, total
+}
+
+// 删除评论(单个评论)
+func DeleteCom(data *ArticleComments) int {
+	err := model.DeleteCom(data.Comid)
+	if err != errmsg.SUCCESS {
+		return errmsg.DeleteCommentFailed
+	}
+	return errmsg.SUCCESS
+}
+
+// 删除评论(某篇文章的所有评论)
+func DeleteAllCom(articleid int64) int {
+	err := model.DeleteAllCom(articleid)
+	if err != errmsg.SUCCESS {
+		return errmsg.DeleteCommentFailed
+	}
+	return errmsg.SUCCESS
+}
