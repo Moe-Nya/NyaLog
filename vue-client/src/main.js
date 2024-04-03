141bbea2120a2a08011a4522ef1ea6598f4e5cc0
MoeNya <i@moenya.cat>
2024-04-03 11:27:45 +0800

    block warning message

--- vue-client/src/main.js
+++ vue-client/src/main.js
@@ -7,11 +7,14 @@ import 'vfonts/FiraCode.css'
 import naive from 'naive-ui/es/preset'
 import axios from 'axios'
 
-axios .defaults.baseURL = 'http://localhost:8080/api/v1'
+axios.defaults.baseURL = 'http://192.168.31.145:8080/api/v1';
+axios.defaults.timeout = 30 * 1000;
 
 const app = createApp(App)
 app.use(router)
 app.use(pinia)
 app.use(naive)
-
+// 屏蔽控制台的警告信息
+app.config.warnHandler = (msg, vm, trace) => {
+};
 app.mount('#app')
